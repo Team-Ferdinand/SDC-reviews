2335124e4d19f714ddd2073bb84a0e52366c621f
Phil Huynh <philiphuynh98@gmail.com>
2021-11-24 12:30:15 -0500

    API Completed

---
+++ server/database/index.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const db = require('../../dbConfig.js');
+
+const pool = new Pool(db.dbconfig);
+
+pool.connect(() => {
+  console.log('connected to db');
+});
+
+module.exports.pool = pool;
---
+++ server/database/models/index.js
@@ -0,0 +1,2 @@
+exports.reviews = require('./reviews.js');
+
