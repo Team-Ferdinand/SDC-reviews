8c5775c651746f7a3a99885fc03c924be4a67d68
Phil Huynh <philiphuynh98@gmail.com>
2021-11-30 19:54:25 -0500

    uninstalled morgan

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const db = require('./database');
 const models = require('./database/models');
-const morgan = require('morgan');
 
 const port = 3000;
 
 app.use(express.json());
-app.use(morgan("dev"));
 
 app.listen(port, () => {
   console.log(`Reviews API server listening at port: ${port}`);
